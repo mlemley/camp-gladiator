c2974549529919608927f56cfb85b675eb97906d
Michael Lemley <michael.lemley@coinninja.com>
2020-02-14 15:21:25 -0600

    initialize dependency injection

--- app/src/androidTest/java/app/camp/gladiator/CampGladiatorApplication.kt
+++ app/src/androidTest/java/app/camp/gladiator/CampGladiatorApplication.kt
@@ -1,12 +1,18 @@
 package app.camp.gladiator
 
 import android.app.Application
+import app.camp.gladiator.di.campGladiatorModules
+import org.koin.core.context.loadKoinModules
 
 
 class CampGladiatorApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        loadDependencyInjection()
+    }
 
+    protected fun loadDependencyInjection() {
+        loadKoinModules(campGladiatorModules)
     }
 }
