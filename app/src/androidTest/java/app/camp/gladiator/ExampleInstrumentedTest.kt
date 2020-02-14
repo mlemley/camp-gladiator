2b21d528f4b9cdfb8c642b6cb021df96672f775d
Michael Lemley <michael.lemley@coinninja.com>
2020-02-14 14:20:51 -0600

    initial theming

--- app/src/androidTest/java/app/camp/gladiator/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/app/camp/gladiator/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package app.campgladiator
+package app.camp.gladiator
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
---
+++ app/src/main/java/app/camp/gladiator/ui/welcome/WelcomeActivity.kt
@@ -0,0 +1,7 @@
+package app.camp.gladiator.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+
+class WelcomeActivity : AppCompatActivity() {
+
+}
