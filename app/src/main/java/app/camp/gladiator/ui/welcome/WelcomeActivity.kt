fa1227d302e9fbbf1b8904838d70031aded21619
Michael Lemley <michael.lemley@coinninja.com>
2020-02-14 15:07:39 -0600

    create cover screen

--- app/src/main/java/app/camp/gladiator/ui/welcome/WelcomeActivity.kt
+++ app/src/main/java/app/camp/gladiator/ui/welcome/WelcomeActivity.kt
@@ -1,7 +1,14 @@
 package app.camp.gladiator.ui.welcome
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import app.camp.gladiator.R
 
 class WelcomeActivity : AppCompatActivity() {
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
 }
